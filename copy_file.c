b65ac617329ea7e52c6946b20838e709d69fb806
thomasjk5 <43922701+thomasjk5@users.noreply.github.com>
2019-05-15 00:13:25 +0530

    Add files via upload

---
+++ copy_file.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char ch;
+	FILE *f1,*f2;
+	f1=fopen("file1.txt","r");   /*I entered hello how are you inside the file*/
+	f2=fopen("file2.txt","a");
+	if(f1==NULL||f2==NULL)
+	printf("File not found");
+	else
+	{
+	while(ch!=EOF)
+	{
+		ch=getc(f1);
+		putc(ch,f2);
+	}
+	printf("File copied Succesfully");
+	fclose(f1);
+	fclose(f2);
+	}
+return 0;
+ }
